9e8fea4c103024fac4a59c301d168e90de8d9304
Satyam-R4j <satyamraj418@gmail.com>
2025-06-13 00:01:49 +0530

    Worked on bottom and text parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
 
 const App = () => {
   const [showContent, setShowContent] = useState(false); // Start with SVG overlay
@@ -32,7 +33,11 @@ const App = () => {
     <>
       {/* SVG Overlay */}
       <div className="svg-overlay fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-black">
-        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice" className="w-full h-full">
+        <svg
+          viewBox="0 0 800 600"
+          preserveAspectRatio="xMidYMid slice"
+          className="w-full h-full"
+        >
           <defs>
             <mask id="viMask">
               <rect width="100%" height="100%" fill="black" />
@@ -65,25 +70,51 @@ const App = () => {
       {showContent && (
         <div className="main w-full">
           <div className="landing w-full h-screen bg-black">
-          <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10 ">
-            <div className="logo flex  gap-10">
-              <div className="lines flex flex-col  gap-1">
-                <div className="line w-10 h-2 bg-white"></div>
-                <div className="line w-8 h-2 bg-white"></div>
-                <div className="line w-5 h-2 bg-white"></div>
-
+            <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10 ">
+              <div className="logo flex  gap-7">
+                <div className="lines flex flex-col  gap-[5px]">
+                  <div className="line w-15 h-2 bg-white"></div>
+                  <div className="line w-8 h-2 bg-white"></div>
+                  <div className="line w-5 h-2 bg-white"></div>
+                </div>
+                <h3 className="text-4xl -mt-[8px] leading-none text-white">
+                  Rockstar
+                </h3>
               </div>
-              <h3 className="text-3xl text-white">Rockstar</h3>
             </div>
-          </div>
+
             <div className="imagesdiv relative overflow-hidden w-full h-screen">
-              <img className="absolute left-0 right-0 w-full h-full object-cover" src="/sky.png" alt="" />
+              <img
+                className="absolute left-0 right-0 w-full h-full object-cover"
+                src="/sky.png"
+                alt=""
+              />
               <img
                 className="absolute left-0 right-0 w-full h-full object-cover"
                 src="/bg.png"
                 alt="Background"
               />
-              <img className="absolute -bottom-[25%] left-1/2 -translate-x-1/2 h-screen scale-[1.4]  "  src="/girlbg.png" alt="" />
+              <div className="text text-white flex flex-col gap-3 absolute   top-20 left-1/2 -translate-x-1/2">
+                <h1 className="text-[12rem] leading-none -ml-40">grand</h1>
+                <h1 className="text-[12rem] leading-none -ml-20">theft</h1>
+                <h1 className="text-[12rem] leading-none -ml-40">auto</h1>
+              </div>
+              <img
+                className="absolute -bottom-[25%] left-1/2 -translate-x-1/2 h-screen scale-[1.4]  "
+                src="/girlbg.png"
+                alt=""
+              />
+            </div>
+            <div className="btmbar text-white absolute bottom-0 left-0 w-full py-15 px-10 bg-gradient-to-t from-black to-transparent">
+              <div className="flex gap-4">
+                <i className="text-4xl ri-arrow-down-line"></i>
+                <h3 className="text-xl font-[monospace]">Scroll Down</h3>
+              </div>
+              <img
+                className="absolute h-[55px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                src="./ps5.png"
+                alt=""
+              />
             </div>
           </div>
         </div>
